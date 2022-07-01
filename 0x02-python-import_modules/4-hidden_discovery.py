9ac30d49c666b579ccfe6358c34f77137d5f2327
tadessewube <softwareteam68@gmail.com>
2022-07-01 11:23:12 +0300

    hidden_discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-import hidden_4
-
-if __name__ == '__main__':
-    def_names = dir(hidden_4)
-
-    for i in range(len(def_names)):
-        if def_names[i][:2] != '__':
-            print(def_names[i])
+if __name__ == "__main__":
+    import hidden_4
+    for s in dir(hidden_4):
+        if s[:2] != "__":
+            print("{:s}".format(s))
