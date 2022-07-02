783f141179ce1b6fcbd5257c2cbf8aab4ae77a5b
tadessewube <softwareteam68@gmail.com>
2022-07-02 09:19:57 +0300

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,15 @@
-#!/usr/bin/pytthon3
-def no_of_argu(*args):
-     
-    # using len() method in args to count
-    return(len(args))
- 
- 
-a = 1
-b = 3
- 
-# arguments passed
-n = no_of_argu(1, 2, 4, a)
- 
-# result printed
-print(" The number of arguments are: ", n)
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the number of and list of arguments."""
+    import sys
+
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
