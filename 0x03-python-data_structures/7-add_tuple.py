66b3f01c0f536f5136f32af4052c0034ee699f68
tadessewube <softwareteam68@gmail.com>
2022-07-02 16:29:41 +0300

    tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    new_tuple = ()
-    tuple_1 = tuple_a + (0, 0)
-    tuple_2 = tuple_b + (0, 0)
-    new_tuple = tuple_1[0] + tuple_2[0], tuple_1[1] + tuple_2[1]
-    return new_tuplet
-
+    # If too long, cut the tuple to the first 2 elements
+    # if too short, extend the tuple to match length 2
+    a = tuple_a[:2] + (0,) * (2 - len(tuple_a))
+    b = tuple_b[:2] + (0,) * (2 - len(tuple_b))
+    c = [x + y for x, y in zip(a, b)]
+    return tuple(c[0:2])
