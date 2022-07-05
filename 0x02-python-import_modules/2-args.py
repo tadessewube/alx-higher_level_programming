76bc2e86b5d3079b7b651714596ed0d40d12743b
tadessewube <softwareteam68@gmail.com>
2022-07-05 01:08:44 -0700

    infinite

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     import sys
 
-    cnt = len(sys.argv) - 1
-    if cnt == 0:
-        print("0 arguments.")
-    elif cnt == 1:
+    length = (len(sys.argv) - 1)
+
+    if length == 1:
         print("1 argument:")
+    elif length == 0:
+        print("0 arguments.")
     else:
-        print("{} arguments:".format(cnt))
-    for i in range(cnt):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print("{} arguments:".format(length))
+
+    for x in range(1, len(sys.argv)):
+        print("{:d}:".format(x), str(sys.argv[x]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+import sys
 if __name__ == "__main__":
-    import sys
+    n = len(sys.argv)
     sum = 0
-    for i in range(1, len(sys.argv)):
+    for i in range(1, n):
         sum += int(sys.argv[i])
     print(sum)
