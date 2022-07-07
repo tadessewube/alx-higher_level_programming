841b1f709c1b7f8562518acc09b0e42c3f846e63
tadessewube <softwareteam68@gmail.com>
2022-07-07 15:14:20 +0300

    0-main

--- 0x04-python-more_data_structures/0-main.py
+++ 0x04-python-more_data_structures/0-main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-multiply_by_2 = __import__('9-multiply_by_2').multiply_by_2
-print_sorted_dictionary = \
-    __import__('6-print_sorted_dictionary').print_sorted_dictionary
+square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
 
-a_dictionary = {'John': 12, 'Alex': 8, 'Bob': 14, 'Mike': 14, 'Molly': 16}
-new_dict = multiply_by_2(a_dictionary)
-print_sorted_dictionary(a_dictionary)
-print("--")
-print_sorted_dictionary(new_dict)
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+new_matrix = square_matrix_simple(matrix)
+print(new_matrix)
+print(matrix)
