a9efd810932301fb9385def4107199f7572e3ad3
tadessewube <softwareteam68@gmail.com>
2022-10-04 16:57:24 +0300

    100-maps

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-let list = require('./100-data.js').list;
+
+const list = require('./100-data').list;
+
 console.log(list);
-let newList = list.map((x, index) => x * index);
-console.log(newList);
+console.log(list.map((x, i) => x * i));
