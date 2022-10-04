d653775733cf8232f017b7faa5155f3b42628d36
tadessewube <softwareteam68@gmail.com>
2022-10-04 17:01:41 +0300

    101-sorted

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-
-const dict = require('./101-data').dict;
-const output = {};
-
-for (const entry in dict) {
-  if (!output[dict[entry]]) output[dict[entry]] = [];
-  output[dict[entry]].push(entry);
+let dict = require('./101-data.js').dict;
+let newDict = {};
+for (let key in dict) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [];
+  }
+  newDict[dict[key]].push(key);
 }
-
-console.log(output);
+console.log(newDict);
