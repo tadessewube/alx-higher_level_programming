36d36be84648ca688a14025c9fc11f88c4b4ff7b
tadessewube <softwareteam68@gmail.com>
2022-10-04 16:38:37 +0300

    7-occurrences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
+
 exports.nbOccurences = function (list, searchElement) {
-  let count = 0;
-  for (let i in list) {
-    if (list[i] === searchElement) {
-      count++;
-    }
-  }
-  return count;
+  return list.reduce((count, item) =>
+    (item === searchElement ? count + 1 : count), 0
+  );
 };
