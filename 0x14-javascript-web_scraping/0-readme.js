6419688ef737c3dba49ac3594a3ad7db8c9c273d
tadessewube <softwareteam68@gmail.com>
2022-11-15 16:17:56 +0300

    readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,12 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, data) {
-  console.log(error || data);
-});
+
+if (process.argv.length > 2) {
+  fs.readFile(process.argv[2], (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data.toString('utf-8'));
+    }
+  });
+}
